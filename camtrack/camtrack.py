be078f4452e2f7121f26c49863944ab24ca31c9d
Sergei Kozelko <kozelkos@yandex.ru>
2022-04-21 22:08:57 +0300

    Run bundle adjustment only if we don't have too many points

--- camtrack/camtrack.py
+++ camtrack/camtrack.py
@@ -171,13 +171,15 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
     # view_mats[known_view_1[0]] = vm_1
     # view_mats[known_view_2[0]] = vm_2
 
-    run_bundle_adjustment(
-        intrinsic_mat,
-        corner_storage,
-        MAX_REPROJ_ERROR,
-        view_mats,
-        point_cloud_builder,
-    )
+    # run bundle adjustment only if we don't have too many points
+    if point_cloud_builder.points.shape[0] < 5000:
+        run_bundle_adjustment(
+            intrinsic_mat,
+            corner_storage,
+            MAX_REPROJ_ERROR,
+            view_mats,
+            point_cloud_builder,
+        )
 
     calc_point_cloud_colors(
         point_cloud_builder,
